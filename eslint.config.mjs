5adbad7fece041ea517176cdfde2e536ab2cf405
danny nsengimana <59091829+dannynsengimana@users.noreply.github.com>
2025-04-13 12:25:23 +0200

    added backend unit tests -01

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,12 +6,28 @@ export default [
   {
     languageOptions: {
       globals: {
-        console: 'readonly',  
+        console: 'readonly',  // Apply console globally
+        process: 'readonly',  // Make 'process' available globally for backend files
       },
     },
     rules: {
       semi: ['error', 'always'],
       quotes: ['error', 'single'],
+    },
+  },
+  {
+    files: ['*.test.js'],  // Limit to test files
+    languageOptions: {
+      globals: {
+        expect: 'readonly',
+        describe: 'readonly',
+        test: 'readonly',
+        it: 'readonly',  // Add 'it' to Jest globals
+        beforeAll: 'readonly',
+        beforeEach: 'readonly',
+        afterAll: 'readonly',
+        afterEach: 'readonly',
+      },
     },
   },
 ];
